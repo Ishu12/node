d9bc0fccbfdcafbcfa12b22b9e22dffcba072c91
Ishu12 <ishu1111@in.ibm.com>
2021-12-02 14:54:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// import express (after npm install express)
+const express = require('express');
 
+// create new express app and save it as "app"
+const app = express();
+
+// server configuration
+const PORT = 8080;
+
+// create a route for the app
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello. Breast cancer death rates declined 40% from 1989 to 2016 among women.<p><a href="https://www.nationalbreastcancer.org/early-detection-of-breast-cancer/">The progress is attributed to improvements in early detection.</a>');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// make the server listen to requests
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});
